dcdaa8e3c284465f8c0392663a90211926371c02
ThanhNee <nbthanh93@gmail.com>
2024-08-02 07:19:57 +0700

    add vue-app

---
+++ vue-app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 10px;
+}
+</style>
